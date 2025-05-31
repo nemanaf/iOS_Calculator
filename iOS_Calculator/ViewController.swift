a182fe9b5842bf14c805a6fede34ded6881ea22b
Manaf <1205manaf@gmail.com>
2025-05-31 21:25:01 +0400

    Initial Commit

---
+++ iOS_Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_Calculator
+//
+//  Created by Manaf A. on 31.05.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
