5a572630f58144bd01f9bf5382637c60df3990b4
Manaf <1205manaf@gmail.com>
2025-06-01 10:50:13 +0400

    Implement digit input logic

--- iOS_Calculator/ViewController.swift
+++ iOS_Calculator/ViewController.swift
@@ -8,17 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentTitle: String = "0"
 
     @IBOutlet weak var displayLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        displayLabel.text = "0"
     }
 
     @IBAction func actionButton(_ sender: UIButton) {
         guard let title = sender.currentTitle else { return }
             print("Button pressed: \(title)")
+        
+        if "0"..."9" ~= title {
+            if currentTitle == "0" {
+                currentTitle = title
+            } else {
+                currentTitle += title
+            }
+            displayLabel.text = currentTitle
+        }
     }
 }
 
