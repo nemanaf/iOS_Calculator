c39e7811324b82ab8917f86fb61f7109df0bc3ad
Manaf <1205manaf@gmail.com>
2025-06-01 11:04:30 +0400

    Implement operation selection and input reset

--- iOS_Calculator/ViewController.swift
+++ iOS_Calculator/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 class ViewController: UIViewController {
     
     var currentTitle: String = "0"
+    var firstTitle: String = ""
+    var currentOperation: Operation? = nil
+    
+    enum Operation {
+        case add
+        case subtract
+        case multiply
+        case divide
+    }
 
     @IBOutlet weak var displayLabel: UILabel!
     
@@ -31,6 +40,28 @@ class ViewController: UIViewController {
             }
             displayLabel.text = currentTitle
         }
+        
+        switch title {
+        case "+":
+            firstTitle = currentTitle
+            currentOperation = .add
+            currentTitle = "0"
+        case "-":
+            firstTitle = currentTitle
+            currentOperation = .subtract
+            currentTitle = "0"
+        case "×":
+            firstTitle = currentTitle
+            currentOperation = .multiply
+            currentTitle = "0"
+        case "÷":
+            firstTitle = currentTitle
+            currentOperation = .divide
+            currentTitle = "0"
+        default:
+            break
+        }
+        displayLabel.text = currentTitle
     }
 }
 
